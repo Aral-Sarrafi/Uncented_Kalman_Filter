74fcbfe5e5e038c78fcd13eea14ddc53202ad57a
Aral Sarrafi <33207285+Aral-Sarrafi@users.noreply.github.com>
2018-08-02 15:19:01 -0400

    Debug prediction function.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -68,6 +68,8 @@ UKF::UKF() {
   weights_.fill(0.5/(lambda_ + n_aug_));
   weights_(0) = lambda_ / (lambda_ + n_aug_);
 
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
+
 }
 
 UKF::~UKF() {}
@@ -183,37 +185,50 @@ void UKF::Prediction(double delta_t) {
 	// Sigma point prediction
 	double delta_t2 = delta_t * delta_t;
 
-	MatrixXd Xsig_pred = MatrixXd(n_x_, 2 * n_aug_ + 1);
 
 
-	for (int i = 0; i< 2 * n_aug_ + 1; i++) {
-
-		double px = Xsig_aug(0, i);
-		double py = Xsig_aug(1, i);
+	for (int i = 0; i< 2 * n_aug_ + 1; i++)
+	{
+		//extract values for better readability
+		double p_x = Xsig_aug(0, i);
+		double p_y = Xsig_aug(1, i);
 		double v = Xsig_aug(2, i);
-		double si = Xsig_aug(3, i);
-		double sidot = Xsig_aug(4, i);
-		double va = Xsig_aug(5, i);
-		double vsidd = Xsig_aug(6, i);
+		double yaw = Xsig_aug(3, i);
+		double yawd = Xsig_aug(4, i);
+		double nu_a = Xsig_aug(5, i);
+		double nu_yawdd = Xsig_aug(6, i);
 
-		if (abs(sidot) == 0)
-		{
-			Xsig_pred(0, i) = px + v * cos(si)*delta_t + 0.5*delta_t2*cos(si)*va;
-			Xsig_pred(1, i) = py + v * sin(si)*delta_t + 0.5*delta_t2*sin(si)*va;
-			Xsig_pred(2, i) = v + delta_t * va;
-			Xsig_pred(3, i) = si + sidot * delta_t + 0.5*delta_t2*vsidd;
-			Xsig_pred(4, i) = sidot + delta_t * vsidd;
+		//predicted state values
+		double px_p, py_p;
+
+		//avoid division by zero
+		if (fabs(yawd) > 0.001) {
+			px_p = p_x + v / yawd * (sin(yaw + yawd * delta_t) - sin(yaw));
+			py_p = p_y + v / yawd * (cos(yaw) - cos(yaw + yawd * delta_t));
 		}
-		else
-		{
-			Xsig_pred(0, i) = px + (v / sidot)*(sin(si + sidot * delta_t) - sin(si)) + 0.5*delta_t2*cos(si)*va;
-			Xsig_pred(1, i) = py + (v / sidot)*(-cos(si + sidot * delta_t) + cos(si)) + 0.5*delta_t2*sin(si)*va;
-			Xsig_pred(2, i) = v + delta_t * va;
-			Xsig_pred(3, i) = si + sidot * delta_t + 0.5*delta_t2*vsidd;
-			Xsig_pred(4, i) = sidot + delta_t * vsidd;
+		else {
+			px_p = p_x + v * delta_t*cos(yaw);
+			py_p = p_y + v * delta_t*sin(yaw);
 		}
 
+		double v_p = v;
+		double yaw_p = yaw + yawd * delta_t;
+		double yawd_p = yawd;
 
+		//add noise
+		px_p = px_p + 0.5*nu_a*delta_t*delta_t * cos(yaw);
+		py_p = py_p + 0.5*nu_a*delta_t*delta_t * sin(yaw);
+		v_p = v_p + nu_a * delta_t;
+
+		yaw_p = yaw_p + 0.5*nu_yawdd*delta_t*delta_t;
+		yawd_p = yawd_p + nu_yawdd * delta_t;
+
+		//write predicted sigma point into right column
+		Xsig_pred_(0, i) = px_p;
+		Xsig_pred_(1, i) = py_p;
+		Xsig_pred_(2, i) = v_p;
+		Xsig_pred_(3, i) = yaw_p;
+		Xsig_pred_(4, i) = yawd_p;
 	}
 
 	// Predic the mean and the covariance matrix
@@ -231,13 +246,13 @@ void UKF::Prediction(double delta_t) {
 	//predict state mean
 	for (int i = 0; i < 2 * n_aug_ + 1; i++)
 	{
-		x += weights_(i)*Xsig_pred.col(i);
+		x += weights_(i)*Xsig_pred_.col(i);
 	}
 
 	//predict state covariance matrix
 	for (int i = 0; i < 2 * n_aug_ + 1; i++)
 	{
-		P += weights_(i)*(Xsig_pred.col(i) - x)*(Xsig_pred.col(i) - x).transpose();
+		P += weights_(i)*(Xsig_pred_.col(i) - x)*(Xsig_pred_.col(i) - x).transpose();
 	}
 	
 	// Update the state vectors and covariance matrix
@@ -259,6 +274,10 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
 
   You'll also need to calculate the lidar NIS.
   */
+	float px = meas_package.raw_measurements_[0];
+	float py = meas_package.raw_measurements_[1];
+
+
 }
 
 /**
@@ -274,4 +293,10 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
 
   You'll also need to calculate the radar NIS.
   */
+	float rho = meas_package.raw_measurements_[0];
+	float phi = meas_package.raw_measurements_[1];
+	float rho_dot = meas_package.raw_measurements_[2];
+
+
+
 }
